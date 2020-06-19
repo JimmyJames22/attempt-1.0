004800c46efbc5e6a9f60e7a876b422b9ebac02c
Duckasaur <zshleifie38@gmail.com>
2020-06-19 15:17:04 -0400

    boom boom

--- src/App.vue
+++ src/App.vue
@@ -26,3 +26,17 @@ export default {
   margin-top: 60px;
 }
 </style>
+
+
+---HOMESCREEN---
+(Arrow keys) hover on deck ---> EMPHASIZE DECK
+(alt + N) create deck --> NAME DECK
+study/edit deck for each deck button
+(alt + F) search decks 
+---NAME DECK---
+input box for deck name
+input box for description (optional)
+cancel button ---> HOME SCREEN
+(ENTER) create button ---> EDIT DECK
+
+---EDIT DECK (LIST VIEW)---
